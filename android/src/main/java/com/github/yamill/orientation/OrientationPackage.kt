83edda6f5d5a4ee37d7d7909bb162c7ffbfb99e1
Amandeep Singh <4373099+mavericksunny@users.noreply.github.com>
2024-08-13 13:26:19 +0530

    Remove abstract

--- android/src/main/java/com/github/yamill/orientation/OrientationPackage.kt
+++ android/src/main/java/com/github/yamill/orientation/OrientationPackage.kt
@@ -5,8 +5,7 @@ import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ViewManager
 
-@Suppress("unused")
-abstract class OrientationPackage : ReactPackage {
+class OrientationPackage : ReactPackage {
 
     override fun createNativeModules(reactContext: ReactApplicationContext) =
         listOf<NativeModule>(
